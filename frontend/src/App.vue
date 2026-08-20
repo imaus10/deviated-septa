6770d69bd3350a097c31f07601df778891e8baa5
Austin Blanton <imaus10@gmail.com>
2026-08-20 13:32:07 -0400

    test deploy

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@ import RouteTable from "./components/RouteTable.vue";
 import RouteMap from "./components/RouteMap.vue";
 import TopStops from "./components/TopStops.vue";
 
+// test deploy
 const { rows, routeGeometries, dataRange, period, loading, error } = useDashboardData();
 
 const routes = computed(() =>
